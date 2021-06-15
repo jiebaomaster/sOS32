fab290c60dedd48e3aa7952bd792223fc6f155fa
jbmaster <jbmaster@hdu.edu.cn>
2021-06-15 06:05:51 -0700

    [add]添加中断处理的基本框架

--- init/entry.c
+++ init/entry.c
@@ -1,3 +1,4 @@
+#include "idt.h"
 #include "gdt.h"
 #include "console.h"
 #include "debug.h"
@@ -5,11 +6,15 @@
 int kern_entry() {
   init_debug();
   init_gdt();
+  init_idt();
 
   console_clear();
   console_write_color("Hello world!\n", rc_black, rc_green);
   
   // panic("test");
+  // 手动触发两个中断
+  asm volatile ("int $0x3");
+  asm volatile ("int $0x4");
 
   return 0;
 }
